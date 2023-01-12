c659bb3f28783d591893202e6dbd067ca2657a1f
Aung Zar Myo <aungzar@Aungs-MacBook-Pro-2.local>
2023-01-12 14:00:48 +0630

    Convert date format

--- app/src/main/java/com/sevenpeakssoftware/aungzarmyo/car_list/CarListAdapter.kt
+++ app/src/main/java/com/sevenpeakssoftware/aungzarmyo/car_list/CarListAdapter.kt
@@ -1,11 +1,15 @@
 package com.sevenpeakssoftware.aungzarmyo.car_list
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.sevenpeakssoftware.aungzarmyo.R
 import com.sevenpeakssoftware.aungzarmyo.databinding.ItemListCarsLayoutBinding
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 class CarListAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -43,10 +47,27 @@ class CarListAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView.V
         fun onBind(carModel: CarModel?) {
             binding.tvTitle.text = carModel?.title
             binding.tvIngress.text = carModel?.ingress
-            binding.tvDate.text = carModel?.dateTime
 
-            // Need to calculate date format
-           // val simpleDateFormat = SimpleDateFormat("dd.MMMM.yyyy ", Locale.US)
+            if (!carModel?.dateTime.isNullOrEmpty()) {
+                try {
+                    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+                    val simpleDateFormat = SimpleDateFormat("dd.mm.yyyy HH:mm", Locale.getDefault())
+                    val currentYearFormat = SimpleDateFormat("dd MMMM, HH:mm", Locale.getDefault())
+                    val differentYearHourFormat = SimpleDateFormat("dd MMMM yyyy, HH:mm", Locale.getDefault())
+                    val carModelDate = simpleDateFormat.parse(carModel?.dateTime!!)
+                    if (carModelDate != null) {
+                        val calendar = Calendar.getInstance()
+                        calendar.time = carModelDate
+                        if (calendar.get(Calendar.YEAR) == currentYear) {
+                            binding.tvDate.text = currentYearFormat.format(carModelDate)
+                        } else {
+                            binding.tvDate.text = differentYearHourFormat.format(carModelDate)
+                        }
+                    }
+                } catch (e: ParseException) {
+                    Log.e("TAG", e.message.toString())
+                }
+            }
 
             Glide.with(binding.root.context)
                 .load(carModel?.image)
