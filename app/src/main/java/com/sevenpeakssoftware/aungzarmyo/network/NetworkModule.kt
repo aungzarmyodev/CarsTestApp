275130a0ccbbef107e7b39e342a0c7a0ceb4218c
Aung Zar Myo <aungzar@Aungs-MacBook-Pro-2.local>
2023-01-14 18:35:50 +0630

    Add OKHTTP client

--- app/src/main/java/com/sevenpeakssoftware/aungzarmyo/network/NetworkModule.kt
+++ app/src/main/java/com/sevenpeakssoftware/aungzarmyo/network/NetworkModule.kt
@@ -5,8 +5,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 
@@ -18,8 +21,17 @@ object NetworkModule {
     @Singleton
     fun provideNetworkService(): CarApi {
 
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(interceptor).build()
+
         return Retrofit.Builder().baseUrl(Const.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(CarApi::class.java)
     }
